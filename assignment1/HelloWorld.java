52cc162036b95abc4f9050b9814b06898ce68a80
sivaleonardo <sivaeinstein@hotmail.com>
2019-06-11 08:56:43 +0530

    Update HelloWorld.java

--- assignment1/HelloWorld.java
+++ assignment1/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args){
-        //Prints Hello, World in terminal window
+        // Prints Hello, World in terminal window
         System.out.println("Hello, World");
     }
 }
